f11a3333bc16378599d20274f4032c0396f2f567
Ivan Timoshenko <tis1993@mail.ru>
2023-04-25 19:46:22 +0200

    Setup theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const theme = require('./theme.json');
+
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -6,7 +9,6 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/layout/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-  },
+  theme,
   plugins: [],
 }
